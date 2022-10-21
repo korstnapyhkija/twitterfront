be89483ce134048da8c0dff038c7a35fa9b7bf91
Annabel Nigu <MM-20-627@tptlive.ee>
2022-10-21 14:46:22 +0300

    ye

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="navbar bg-neutral text-neutral-content">
+  <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl">Twitter</a>
+  </div>
+</div>
+  <div class="container mx-auto px-20">
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div v-if="tweets" v-for="tweet in tweets.data" :key="tweet.id" class="card bg-base-100 shadow-xl mb-3">
+        <div class="card-body">
+            <div class="flex flex-row">
+                <div>
+                    <div class="avatar">
+                        <div class="w-16 rounded-full">
+                            <img :src="tweet.user.profile.image" />
+                        </div>
+                    </div>
+           
+                </div>
+                <div class="ml-3">
+                    <b>{{tweet.user.name}}</b>&nbsp;&nbsp;
+                    <span class="text-zinc-400">@{{tweet.user.username}}</span>
+                    <p>{{tweet.content}}</p>
+                </div>
+            </div>
+           
+           
+            <div class="divider"></div>
+            <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    created(){
+        axios.get('http://localhost:8000/api/tweets').then(response => {
+            console.log(response);
+            this.tweets = response.data;
+        });
+    },
+    data(){
+        return {
+            tweets: null
+        }
+    }
+}
+</script>
